2a2d0212f620d476f66a9c1744d9d65845f9055e
Splitxorpio <73331288+Splitxorpio@users.noreply.github.com>
2021-06-12 11:44:51 -0700

    Add files via upload
    Version as of 11:44am PDT 6/12/21

---
+++ process.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'db.php';
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
+
+$result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username' and password = '$password'")
+    or die("Failed to query database" . mysqli_error($link));
+$row = mysqli_fetch_array($result);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./phpcss.css">
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <div class="container d50">
+        <div class="form-holder">
+            <span class="emoji">👋</span>
+            <h1>
+                <?php
+                if ($row['username'] == $username && $row['password'] == $password) {
+                    echo " Hello " . $row['username'] . "!";
+                } else {
+                    header("Location:login.php");
+                }
+                ?>
+            </h1>
+            <br>
+            <a class="btn" href="courses.html">Look For Courses!</a>
+        </div>
+    </div>
+</body>
+
+</html>
